56d68b32d435694da9a87bc3834727cc167e91d6
agus-a97 <agustin.f.aguilar97@gmail.com>
2022-01-18 00:19:06 -0300

    creamos itemCount para el desafio

--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { ItemCount } from './itemCount/ItemCount'
 
 export const ItemListContainer = (props) => {
     return (
         <div>
             <h1 className='titulo-1'>{props.gretting}</h1>
+
+            <ItemCount initial={1} stock={5}/>
         </div>
     )
 }
